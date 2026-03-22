71c92303bbe82d129727f1e4c3883d5eaccf78ef
RistoKuntro <kuntro.risto@gmail.com>
2026-03-22 21:04:32 +0200

    Updated prisma schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,11 @@
-import { PrismaClient } from "../generated/prisma/client";
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+
 const prisma = new PrismaClient({
   adapter: new PrismaPg({
     connectionString: process.env.DATABASE_URL as string,
   }),
 });
+
 export default prisma;

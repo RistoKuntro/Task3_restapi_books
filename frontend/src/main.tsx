58648f782899772db7fe0f056f84d5d9a3696aad
RistoKuntro <kuntro.risto@gmail.com>
2026-04-28 23:32:08 +0300

    add React Router with books and book detail routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import BooksPage from './pages/BooksPage'
+import BookDetailPage from './pages/BookDetailPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/books" replace />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/books/:id" element={<BookDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
